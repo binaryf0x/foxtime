3878fb4b6e6636c81e47ef06bd4e9e9e977d43c7
binaryfox <binary@foxontheinter.net>
2025-09-04 19:53:21 -0700

    Send current time in the initial response

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,22 @@ use actix_web::{App, HttpResponse, HttpServer, Responder, get};
 
 #[get("/")]
 async fn index() -> impl Responder {
+    let timestamp = match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
+        Ok(timestamp) => timestamp.as_millis().to_string(),
+        _ => return HttpResponse::InternalServerError().finish(),
+    };
     HttpResponse::Ok()
         .content_type(ContentType::html())
         .append_header((CROSS_ORIGIN_OPENER_POLICY, "same-origin"))
         .append_header((CROSS_ORIGIN_EMBEDDER_POLICY, "require-corp"))
-        .body(include_str!("index.html"))
+        .body(include_str!("index.html").replace("INITIAL_SERVER_TIME", &timestamp))
 }
 
 #[get("/time")]
 async fn time() -> impl Responder {
     match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
-        Ok(timestamp) => timestamp.as_millis().to_string(),
-        _ => "Error".to_string(),
+        Ok(timestamp) => HttpResponse::Ok().body(timestamp.as_millis().to_string()),
+        _ => HttpResponse::InternalServerError().finish(),
     }
 }
 
