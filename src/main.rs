efb43a24f246bb177088eec9fec9b501fa64a135
binaryfox <binary@foxontheinter.net>
2025-09-04 19:53:21 -0700

    Support configuring listen parameters

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,71 @@
-use std::time::SystemTime;
+use std::time::{SystemTime, SystemTimeError};
 
 use actix_web::{App, HttpResponse, HttpServer, Responder, get, http::header, middleware};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, conflicts_with = "unix", default_value_t = String::from("127.0.0.1"))]
+    host: String,
+
+    #[arg(short, long, group = "bind", default_value_t = 8123)]
+    port: u16,
+
+    #[arg(short, long, group = "bind")]
+    unix: Option<String>,
+}
+
+fn timestamp() -> Result<String, SystemTimeError> {
+    let timestamp = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH)?;
+    Ok(format!(
+        "{}.{:03}",
+        timestamp.as_millis(),
+        timestamp.subsec_micros() % 1_000
+    ))
+}
 
 #[get("/")]
 async fn index() -> impl Responder {
-    let timestamp = match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
-        Ok(timestamp) => timestamp.as_millis().to_string(),
-        _ => return HttpResponse::InternalServerError().finish(),
-    };
-    HttpResponse::Ok()
-        .content_type(header::ContentType::html())
-        .insert_header(header::CacheControl(vec![header::CacheDirective::NoStore]))
-        .insert_header((header::CROSS_ORIGIN_OPENER_POLICY, "same-origin"))
-        .insert_header((header::CROSS_ORIGIN_EMBEDDER_POLICY, "require-corp"))
-        .body(include_str!("index.html").replace("INITIAL_SERVER_TIME", &timestamp))
+    match timestamp() {
+        Ok(timestamp) => HttpResponse::Ok()
+            .content_type(header::ContentType::html())
+            .insert_header(header::CacheControl(vec![header::CacheDirective::NoStore]))
+            .insert_header((header::CROSS_ORIGIN_OPENER_POLICY, "same-origin"))
+            .insert_header((header::CROSS_ORIGIN_EMBEDDER_POLICY, "require-corp"))
+            .body(include_str!("index.html").replace("INITIAL_SERVER_TIME", &timestamp)),
+        _ => HttpResponse::InternalServerError().finish(),
+    }
 }
 
 #[get("/time")]
 async fn time() -> impl Responder {
-    let timestamp = match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
-        Ok(timestamp) => format!(
-            "{}.{:03}",
-            timestamp.as_millis(),
-            timestamp.subsec_micros() % 1_000
-        ),
-        _ => return HttpResponse::InternalServerError().finish(),
-    };
-    HttpResponse::Ok()
-        .content_type(header::ContentType::json())
-        .insert_header(header::ContentEncoding::Identity)
-        .insert_header(header::CacheControl(vec![header::CacheDirective::NoStore]))
-        .body(timestamp)
+    match timestamp() {
+        Ok(timestamp) => HttpResponse::Ok()
+            .content_type(header::ContentType::json())
+            .insert_header(header::ContentEncoding::Identity)
+            .insert_header(header::CacheControl(vec![header::CacheDirective::NoStore]))
+            .body(timestamp),
+        _ => HttpResponse::InternalServerError().finish(),
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let args = Args::parse();
+
+    let mut server = HttpServer::new(|| {
         App::new()
             .wrap(middleware::Compress::default())
             .service(index)
             .service(time)
-    })
-    .bind(("0.0.0.0", 8123))?
-    .run()
-    .await
+    });
+
+    if let Some(unix) = args.unix {
+        server = server.bind_uds(unix)?;
+    } else {
+        server = server.bind((args.host.as_str(), args.port))?;
+    }
+
+    server.run().await
 }
