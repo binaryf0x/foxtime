a8601d45f88230a20af4d4c99062d84f25928674
binaryfox <binary@foxontheinter.net>
2025-09-04 19:53:21 -0700

    Explicit cache control and compression

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 use std::time::SystemTime;
 
-use actix_web::http::header::{
-    CROSS_ORIGIN_EMBEDDER_POLICY, CROSS_ORIGIN_OPENER_POLICY, ContentType,
-};
-use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, http::header, middleware};
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -12,24 +9,35 @@ async fn index() -> impl Responder {
         _ => return HttpResponse::InternalServerError().finish(),
     };
     HttpResponse::Ok()
-        .content_type(ContentType::html())
-        .append_header((CROSS_ORIGIN_OPENER_POLICY, "same-origin"))
-        .append_header((CROSS_ORIGIN_EMBEDDER_POLICY, "require-corp"))
+        .content_type(header::ContentType::html())
+        .insert_header(header::CacheControl(vec![header::CacheDirective::NoStore]))
+        .insert_header((header::CROSS_ORIGIN_OPENER_POLICY, "same-origin"))
+        .insert_header((header::CROSS_ORIGIN_EMBEDDER_POLICY, "require-corp"))
         .body(include_str!("index.html").replace("INITIAL_SERVER_TIME", &timestamp))
 }
 
 #[get("/time")]
 async fn time() -> impl Responder {
-    match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
-        Ok(timestamp) => HttpResponse::Ok().body(timestamp.as_millis().to_string()),
-        _ => HttpResponse::InternalServerError().finish(),
-    }
+    let timestamp = match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
+        Ok(timestamp) => timestamp.as_millis().to_string(),
+        _ => return HttpResponse::InternalServerError().finish(),
+    };
+    HttpResponse::Ok()
+        .content_type(header::ContentType::json())
+        .insert_header(header::ContentEncoding::Identity)
+        .insert_header(header::CacheControl(vec![header::CacheDirective::NoStore]))
+        .body(timestamp)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index).service(time))
-        .bind(("127.0.0.1", 8123))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Compress::default())
+            .service(index)
+            .service(time)
+    })
+    .bind(("127.0.0.1", 8123))?
+    .run()
+    .await
 }
